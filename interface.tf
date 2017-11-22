8a73906c1174f283454f538c07ee13247417224b
richardx14 <richard@dicecentre.org>
2017-11-22 19:00:03 +0000

    Initial commit adding s3 bucket module and resource

---
+++ interface.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  default = "eu-west-2"
+  description = "AWS region"
+}
+
+variable "prefix" {
+  default = "dicecentre"
+  description = "name of our org"
+}
+
+variable "environment" {
+  default = "development"
+  description = "name of our env"
+}
+
+output "s3_bucket_id" {
+  value = "${aws_s3_bucket.remote_state_id}"
+}
