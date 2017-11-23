94829cd88b49ef14570c55d2e66249d4acf2bef6
richardx14 <richard@dicecentre.org>
2017-11-23 19:50:48 +0000

    Replaced more _ with -'s

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "remote_state" {
-  bucket = "${var.prefix}-remote_state-${var.environment}"
+  bucket = "${var.prefix}-remote-state-${var.environment}"
   acl = "authenticated-read"
   versioning {
     enabled = true
   }
 
   tags {
-    Name = "${var.prefix}-remote_state-${var.environment}" 
+    Name = "${var.prefix}-remote-state-${var.environment}" 
     Environment = "${var.environment}"
   }
 }
